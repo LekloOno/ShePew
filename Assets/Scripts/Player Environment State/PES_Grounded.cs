686dc043d253df7307e4c7e078b39da3df377362
Leklo <shems.petremand@gmail.com>
2023-02-22 22:34:51 +0100

    Ground Detection Clean
    Cleaned the ground detection script PES_Grounded.cs, first so that it only deals with ground detection, and no longer implements the step climber functionality.
    We will instead make the step climber an independent script which will disable the whole PES_Grounded.cs script when enabled, and set the grounded state to true.
    
    The ground Collider storage and update events have also been removed, since in this new version of the project, we won't be using player material friction, and these were used to deal with the walls friction adaptation.

--- Assets/Scripts/Player Environment State/PES_Grounded.cs
+++ Assets/Scripts/Player Environment State/PES_Grounded.cs
@@ -7,18 +7,14 @@ public class PSS_Ground : MonoBehaviour
 {
     [Header("States")]
     public bool IsGrounded;
-    public bool IsSloped;
-    public Collider GroundCollider;
+
+
     public float GroundHeight;
     public float GroundAngle;
     public Vector3 GroundNormal;
 
     public event EventHandler OnLanding;
     public event EventHandler OnLeavingGround;
-    public event EventHandler OnEnteringSlope;
-    public event EventHandler OnExitingSlope;
-    public event EventHandler OnUpdatingGround;
-    public event EventHandler OnUpdatingGroundCollider;
 
     [Header("Rigidbody")]
     public float FlatVelocity;
@@ -28,32 +24,14 @@ public class PSS_Ground : MonoBehaviour
     [SerializeField] Rigidbody rb;
     [SerializeField] Transform spatial;
     [SerializeField] Transform body;
-    [SerializeField] Transform lowerSphere;
-    [SerializeField] PIS_Combat inputHandler;
     [SerializeField] LayerMask ground;
     [SerializeField] float distToGround;
     [SerializeField] float groundedRadius;
     [SerializeField] float groundedMaxAngle;
-    //[SerializeField] float distTGAngleMultiplier;
-    [Header("Tweak/climber")]
-    [SerializeField] float climbSpeed;
-    [SerializeField] float floorMargin;
-    [SerializeField] float maxHeight;
-    [SerializeField] float maxDistance;
-    [SerializeField] float upperDistanceOffset;
-    [Header("Tweak/Slope")]
-    [SerializeField] float slopeMinAngle;
-    [SerializeField] float slopeMaxAngle;
     [SerializeField] float maxVerticalSpeed;
 
     RaycastHit groundHit;
     RaycastHit groundedHit;
-    float finalDistTG;
-
-    RaycastHit lowerHit;
-    RaycastHit upperHit;
-    Vector3 lowerPoint;
-    Vector3 upperPoint;
 
     [SerializeField] bool isClimbing;
 
@@ -75,113 +53,27 @@ public class PSS_Ground : MonoBehaviour
 
     void FixedUpdate()
     {
-        //if(rb.velocity.y<0) Debug.Log(rb.velocity.y + " | ");
         realDistToGround = transform.localScale.y*distToGround;
         Velocity = rb.velocity;
         FlatVelocity = (new Vector3(Velocity.x,0,Velocity.z)).magnitude;
 
-        if(Physics.SphereCast(spatial.position, groundInfoRadius, Vector3.down, out groundHit, Mathf.Infinity, ground))
-        {
-            GroundHeight = groundHit.distance+groundInfoRadius;
-            if(GroundCollider != groundHit.collider)
-            {
-                OnUpdatingGroundCollider?.Invoke(this,EventArgs.Empty);
-                GroundCollider = groundHit.collider;
-            }
-            OnUpdatingGround?.Invoke(this, EventArgs.Empty);
-        }
-
-        lowerPoint = new Vector3(spatial.position.x, spatial.position.y - 0.9f*transform.localScale.y+floorMargin, spatial.position.z);
-        upperPoint = new Vector3(spatial.position.x, spatial.position.y - 0.9f*transform.localScale.y+maxHeight, spatial.position.z);
-
-        isClimbing = false;
-        if((inputHandler.WishDir != Vector3.zero || (rb.velocity.x != 0 || rb.velocity.z != 0)) && Physics.Raycast(lowerPoint, inputHandler.WishDir, out lowerHit, maxDistance, ground))
-        {
-            GroundNormal = lowerHit.normal;
-            GroundAngle = Vector3.Angle(Vector3.up, lowerHit.normal);
-            if(!Physics.Raycast(upperPoint, inputHandler.WishDir, out upperHit, maxDistance+upperDistanceOffset, ground) && GroundAngle > 45)
-            {
-                rb.position += new Vector3(0f, climbSpeed, 0f);
-                GroundNormal = Vector3.up;
-                GroundAngle = 0;
-                UpdateGrounded(true);
-                isClimbing = true;
-            }
-        }
-        
-        if(!isClimbing)
-        {
-            if(Physics.SphereCast(spatial.position, groundedRadius, Vector3.down, out groundedHit, realDistToGround-groundedRadius, ground))
-            {
-                GroundNormal = groundedHit.normal;
-                GroundAngle = Vector3.Angle(Vector3.up, groundedHit.normal);
-                GroundCollider = groundedHit.collider;
-                UpdateGrounded(GroundAngle<=groundedMaxAngle);
-            }
-            else
-            {
-                GroundNormal = Vector3.up;
-                GroundAngle = 0;
-                UpdateGrounded(false);
-            }
-        }
-
-        UpdateSloped(IsGrounded && GroundAngle <= slopeMaxAngle && GroundAngle >= slopeMinAngle);
-
-        
-        #region Obsolete
-        /*
-        /PREVIOUS RAY CASTING GROUND DETECTION\
-
-
-        if(Physics.Raycast(transform.position, Vector3.down, out groundHit, Mathf.Infinity, ground))
+        if(Physics.Raycast(spatial.position, Vector3.down, out groundHit, Mathf.Infinity, ground))
         {
             GroundHeight = groundHit.distance;
-            //GroundCollider = groundHit.collider;
-            if(groundHit.distance <= finalDistTG+0.3f)
-            {
-                lowerPoint = new Vector3(rb.position.x, rb.position.y - 0.9f+floorMargin, rb.position.z);
-                upperPoint = new Vector3(rb.position.x, rb.position.y - 0.9f+maxHeight, rb.position.z);
+        }
 
-                if(inputHandler.WishDir != Vector3.zero && Physics.Raycast(lowerPoint, inputHandler.WishDir, out lowerHit, maxDistance, ground))
-                {
-                    GroundNormal = lowerHit.normal;
-                    GroundAngle = Vector3.Angle(Vector3.up, lowerHit.normal);
-                    if(!Physics.Raycast(upperPoint, inputHandler.WishDir, out upperHit, maxDistance+upperDistanceOffset, ground) && GroundAngle > 45 && IsGrounded)
-                    {
-                        rb.position += new Vector3(0f, climbSpeed, 0f);
-                        GroundNormal = Vector3.up;
-                        GroundAngle = 0;
-                    }
-                }
-                else
-                {
-                    GroundNormal = groundHit.normal;
-                    GroundAngle = Vector3.Angle(Vector3.up, groundHit.normal);
-                }
-            }
-            else
-            {
-                GroundNormal = Vector3.up;
-                GroundAngle = 0;
-            }
-            
-            
-            finalDistTG = distToGround+Mathf.Min(GroundAngle,45)*distTGAngleMultiplier;
-
-            bool nextGrounded = groundHit.distance <= finalDistTG;
-
-            if(IsGrounded != nextGrounded)
-            {
-                IsGrounded = nextGrounded;
-
-                if(nextGrounded)
-                    OnLanding?.Invoke(this, EventArgs.Empty);
-                else
-                    OnLeavingGround?.Invoke(this, EventArgs.Empty);
-            }
-        }*/
-        #endregion
+        if(Physics.SphereCast(spatial.position, groundedRadius, Vector3.down, out groundedHit, realDistToGround-groundedRadius, ground))
+        {
+            GroundNormal = groundedHit.normal;
+            GroundAngle = Vector3.Angle(Vector3.up, groundedHit.normal);
+            UpdateGrounded(GroundAngle<=groundedMaxAngle);
+        }
+        else
+        {
+            GroundNormal = Vector3.up;
+            GroundAngle = 0;
+            UpdateGrounded(false);
+        }
     }
 
     void OnDrawGizmosSelected()
@@ -204,18 +96,6 @@ public class PSS_Ground : MonoBehaviour
                 OnLanding?.Invoke(this, EventArgs.Empty);
             else
                 OnLeavingGround?.Invoke(this, EventArgs.Empty);
-        }
-    }
-
-    void UpdateSloped(bool nextSloped)
-    {
-        if(IsSloped != nextSloped)
-        {
-            IsSloped = nextSloped;
-            if(nextSloped)
-                OnEnteringSlope?.Invoke(this, EventArgs.Empty);
-            else
-                OnExitingSlope?.Invoke(this, EventArgs.Empty);
         }
     }
 }
