4c92aed26649e2ef7b06cf801b81891b45bdc61b
Leklo <shems.petremand@gmail.com>
2023-05-25 22:04:28 +0200

    Weapon sway Z axis
    Added a Z axis sway on ANIM_WeaponSway

--- Assets/Scripts/Annimation/ANIM_WeaponSway.cs
+++ Assets/Scripts/Annimation/ANIM_WeaponSway.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class ANIM_WeaponSway : MonoBehaviour
 {
     [SerializeField] private PC_Control _cameraControl;
-    [SerializeField] private float _smooth;
+    [SerializeField] private float _reactive;
     [SerializeField] private float _strength;
 
     void Update()
     {
         Quaternion rotationX = Quaternion.AngleAxis(-_cameraControl.LookY * _strength, Vector3.right);
         Quaternion rotationY = Quaternion.AngleAxis(_cameraControl.LookX * _strength, Vector3.up);
+        Quaternion rotationZ = Quaternion.AngleAxis(_cameraControl.LookX * _strength, -Vector3.forward);
 
-        Quaternion targetRotation = rotationX * rotationY;
+        Quaternion targetRotation = rotationX * rotationY * rotationZ;
 
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, _smooth*Time.deltaTime);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, _reactive*Time.deltaTime);
     }
 }
