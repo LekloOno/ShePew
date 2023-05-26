b0bcb70488bf8c0af829df920c021360ad24fc18
Leklo <shems.petremand@gmail.com>
2023-05-26 16:11:29 +0200

    Momentum lurch penalty + neutral fix
    We can now set a momentum lurch (previously named full lurch) penalty.
    Neutral would call a lurch. Lurch are now only called if wishDir is not equal to Vector3.zero.

--- Assets/Scripts/Player Movement/Movement/Actions/Ability/Lurch/PMA_Lurch.cs
+++ Assets/Scripts/Player Movement/Movement/Actions/Ability/Lurch/PMA_Lurch.cs
@@ -11,13 +11,17 @@ public class PMA_Lurch : MonoBehaviour
     [SerializeField] private PES_Grounded _groundState;
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _maxTime;    //Max Time after a jump in Seconds.
-    [SerializeField] private float _maxAngle;   //Max Lurch Angle in Degrees.
+    [SerializeField] private float _maxAngle = 180f;   //Max Lurch Angle in Degrees.
 
-    //Direct Lurch and Full Lurch
-    [SerializeField] private float _maxFullLurchAngle = 180;
-    //Max Angle for a classic Full Lurch. Beyond this angle, the redirection will be completely based on wishDir, but with a loss of speed, a Direct Lurch.
-    [SerializeField] private float _wideLurchPenalty = 1;
-    //The coefficient of a Direct Lurch velocity, 1 means no penalty.
+    [Header("Direct and Momentum Lurch")]
+    [SerializeField] private float _maxFullLurchAngle = 160f;
+    //Max Angle for a Momentum Lurch. Beyond this angle, the redirection will be completely based on wishDir, but with a loss of speed, a Direct Lurch.
+    [SerializeField] private float _wideLurchPenalty = 0.7f;
+    //The velocity coefficient of a Direct Lurch, 1 means no penalty.
+    [SerializeField] private float _momentumLurchPenalty = 0.05f;
+    [SerializeField] private float _maximumMomentumLurchKMPenalty = 5f; //In km/h
+    private float _maximumMomentumLurchPenalty;
+    //The velocity coefficent of a Momentum Lurch, 1 means no penalty.
 
     private float _maxSteps;    //Max Time converted in engine ticks.
     private float _maxDot;      //Max Lurch Angle converted into the max Dot Product.
@@ -26,50 +30,29 @@ public class PMA_Lurch : MonoBehaviour
 
     void Start()
     {
+        _maximumMomentumLurchPenalty = _maximumMomentumLurchKMPenalty/3.6f;
         _maxDot = Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * _maxAngle));
         _maxFullLurch = Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * _maxFullLurchAngle));
         Debug.Log(_maxDot);
         _maxSteps = _maxTime/Time.fixedDeltaTime;
-        //inputsMM.playerInputActions.Arena.Running.performed += Lurch_OnKeyPressed2;
         _runningInput.KeyPressed += Lurch_OnKeyPressed;
-    }
-
-    public void Lurch_OnKeyPressed2(InputAction.CallbackContext obj)
-    {
-        Debug.Log("Pressed" + obj.phase);
-        Vector3 localWishDir = _runningInput.FreeWishDir(obj.ReadValue<Vector2>());
-        float absDot = Mathf.Abs(Vector3.Dot(_groundState.FlatVelocity.normalized, localWishDir));
-        if(!_groundState.IsGrounded && _groundState.StepSinceLastJumped < _maxSteps && absDot<=_maxDot)
-        {
-            if(absDot < _maxFullLurch)
-            {
-                Debug.Log("FULL LURCH");
-                Vector3 lerpedDir = Vector3.Lerp(_groundState.FlatVelocity.normalized, localWishDir, 0.5f).normalized;
-                _rb.velocity = lerpedDir*_groundState.FlatSpeed + new Vector3(0, _rb.velocity.y, 0);
-            }
-            else
-            {
-                Debug.Log("DEBUFF");
-                _rb.velocity = localWishDir.normalized * _groundState.FlatSpeed * _wideLurchPenalty + new Vector3(0, _rb.velocity.y, 0);
-            }
-        }
     }
 
     public void Lurch_OnKeyPressed(object sender, Vector3 wishDir)
     {
-        //wishDir = _runningInput.FreeWishDir(obj.ReadValue<Vector2>());
         float absDot = Mathf.Abs(Vector3.Dot(_groundState.FlatVelocity.normalized, wishDir));
-        if(!_groundState.IsGrounded && _groundState.StepSinceLastJumped < _maxSteps && absDot<=_maxDot)
+        if(!_groundState.IsGrounded && _groundState.StepSinceLastJumped < _maxSteps && absDot<=_maxDot && wishDir != Vector3.zero)
         {
             if(absDot < _maxFullLurch)
             {
-                Debug.Log("FULL LURCH");
+                //MOMENTUM LURCH
                 Vector3 lerpedDir = Vector3.Lerp(_groundState.FlatVelocity.normalized, wishDir, 0.5f).normalized;
-                _rb.velocity = lerpedDir*_groundState.FlatSpeed + new Vector3(0, _rb.velocity.y, 0);
+                float speedPenalty = Mathf.Min(_groundState.FlatSpeed * _momentumLurchPenalty, _maximumMomentumLurchPenalty);
+                _rb.velocity = lerpedDir*(_groundState.FlatSpeed - speedPenalty) + new Vector3(0, _rb.velocity.y, 0);
             }
             else
             {
-                Debug.Log("DEBUFF");
+                //DIRECT LURCH
                 _rb.velocity = wishDir.normalized * _groundState.FlatSpeed * _wideLurchPenalty + new Vector3(0, _rb.velocity.y, 0);
             }
         }
